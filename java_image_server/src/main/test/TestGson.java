0644b09e8fc02e20504f4a50455e12057755fcde
笨蛋呢 <49787011+NEFELIBATA-Reset@users.noreply.github.com>
2020-04-21 20:44:30 +0800

    Update TestGson.java

--- java_image_server/src/main/test/TestGson.java
+++ java_image_server/src/main/test/TestGson.java
@@ -2,6 +2,24 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.HashMap;
 
+/*
+测试GSON
+GSON是Google提供的用来在Java对象和JSON数据之间进行映射的Java类库。可以将一个Json字符转成一个Java对象，或者将一个Java转化为Json字符串。
+两个方法：
+public String toJson(Objcet obj)方法，可以将对象转化为json字符串。
+public T fromJson(String jsonStr,T.class)方法，可以将json字符串转化为Java对象 
+特点： a、快速、高效
+    　　b、代码量少、简洁
+    　　c、面向对象
+       　d、数据传递和解析方便
+ 
+Gson的pom依赖：
+        <dependency>
+            <groupId>com.google.code.gson</groupId>
+            <artifactId>gson</artifactId>
+            <version>2.8.0</version>
+        </dependency>
+*/
 class Hero {
     public String name;
     public String skill1;
@@ -12,6 +30,7 @@ class Hero {
 
 public class TestGson {
     public static void main(String[] args) {
+        //可以用hashMap模拟
 //        HashMap<String, Object> hashMap = new HashMap<>();
 //        hashMap.put("name", "曹操");
 //        hashMap.put("skill1", "剑气");
